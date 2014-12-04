904b205b4658f106d8b69665d5eb2321362d1e03
Witz Hsiao <witz.hsiao@gmail.com>
2014-12-04 18:58:26 +0800

    fix the proxy in curl and use the google play key in AndroidManifest

--- GbombSDKSample/proj.android/jni/Android.mk
+++ GbombSDKSample/proj.android/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := gbombsdk_share
 LOCAL_MODULE_FILENAME := libgbombsdk
-LOCAL_SRC_FILES := ../../libgbombsdk.so
+LOCAL_SRC_FILES := ../../../libgbombsdk.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../header/
 include $(PREBUILT_SHARED_LIBRARY)
 
